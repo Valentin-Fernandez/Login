4ca2fb75ef315b951389d8f9fce52ca95a9edb46
Valentin <dev.valentinfernandez@gmail.com>
2025-03-12 11:13:26 -0300

    URL CHANGE v2

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
-app.use(cors({ origin: 'https://login-psi-gray.vercel.app/', credentials: true }));
+app.use(cors({ origin: 'https://login-psi-gray.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
